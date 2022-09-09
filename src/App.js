b2aa94be3431e45af5110800e09a63cbaf694385
Milad sharifi <milad.sm76@gmail.com>
2022-09-09 13:53:30 +0430

    rename classname

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import Miners from './miners/Miners';
-import Navbar from './miners/Navbar';
 function App() {
   return (
     <div className="App">
-        <Navbar/>
+        
         <Miners/>
     </div>
   );
 }
-
+ 
 export default App;
